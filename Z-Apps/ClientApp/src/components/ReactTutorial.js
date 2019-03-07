ad079ba90106a6c46a5d663a0a3ac48533e01006
Kosuke Zaizen <kosukezaizen@yahoo.co.jp>
2019-03-07 11:42:35 +0900

    Passing Data Through Props

--- Z-Apps/ClientApp/src/components/ReactTutorial.js
+++ Z-Apps/ClientApp/src/components/ReactTutorial.js
@@ -2,20 +2,66 @@ import React from 'react';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import { actionCreators } from '../store/Counter';
+import './ReactTutorial.css';
 
-const Counter = props => (
-  <div>
-    <h1>Counter</h1>
+class Square extends React.Component {
+    render() {
+        return (
+            <button className="square">
+                {this.props.value}
+            </button>
+        );
+    }
+}
 
-    <p>This is a simple example of a React component.</p>
+class Board extends React.Component {
+    renderSquare(i) {
+        return <Square value={i} />;
+    }
 
-    <p>Current count: <strong>{props.count}</strong></p>
+    render() {
+        const status = 'Next player: X';
 
-    <button className="btn btn-primary" onClick={props.increment}>Increment</button>
-  </div>
-);
+        return (
+            <div>
+                <div className="status">{status}</div>
+                <div className="board-row">
+                    {this.renderSquare(0)}
+                    {this.renderSquare(1)}
+                    {this.renderSquare(2)}
+                </div>
+                <div className="board-row">
+                    {this.renderSquare(3)}
+                    {this.renderSquare(4)}
+                    {this.renderSquare(5)}
+                </div>
+                <div className="board-row">
+                    {this.renderSquare(6)}
+                    {this.renderSquare(7)}
+                    {this.renderSquare(8)}
+                </div>
+            </div>
+        );
+    }
+}
+
+class Game extends React.Component {
+    render() {
+        return (
+            <div className="game">
+                <div className="game-board">
+                    <Board />
+                </div>
+                <div className="game-info">
+                    <div>{/* status */}</div>
+                    <ol>{/* TODO */}</ol>
+                </div>
+            </div>
+        );
+    }
+}
 
 export default connect(
   state => state.counter,
   dispatch => bindActionCreators(actionCreators, dispatch)
-)(Counter);
+)(Game);
