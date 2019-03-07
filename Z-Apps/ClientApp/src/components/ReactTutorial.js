d9bd84ea3d98162186bf1323f5f433288f0d8c36
Kosuke Zaizen <kosukezaizen@yahoo.co.jp>
2019-03-07 18:39:01 +0900

    Function Components

--- Z-Apps/ClientApp/src/components/ReactTutorial.js
+++ Z-Apps/ClientApp/src/components/ReactTutorial.js
@@ -4,18 +4,15 @@ import { connect } from 'react-redux';
 import { actionCreators } from '../store/Counter';
 import './ReactTutorial.css';
 
-class Square extends React.Component {
-
-    render() {
-        return (
-            <button
-                className="square"
-                onClick={() => this.props.onClick()}
-            >
-                {this.props.value}
-            </button>
-        );
-    }
+function Square(props) {
+    return (
+        <button
+            className="square"
+            onClick={props.onClick}
+        >
+            {props.value}
+        </button>
+    );
 }
 
 class Board extends React.Component {
