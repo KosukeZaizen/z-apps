ee4cb77bed09f8dab28f7f9b22a76de8068390d2
Kosuke Zaizen <kosukezaizen@yahoo.co.jp>
2019-03-11 19:29:40 +0900

    add basic functions of Romaji Converter

--- Z-Apps/ClientApp/src/components/RomajiConverter.js
+++ Z-Apps/ClientApp/src/components/RomajiConverter.js
@@ -4,6 +4,17 @@ import { connect } from 'react-redux';
 import { actionCreators } from '../store/Counter';
 import '../css/RomajiConverter.css';
 
+const objTwoChars = { "きゃ": "kya", "きゅ": "kyu", "きょ": "kyo", "しゃ": "sha", "しゅ": "shu", "しょ": "sho", "ちゃ": "cha", "ちゅ": "chu", "ちょ": "cho", "にゃ": "nya", "にゅ": "nyu", "にょ": "nyo", "ひゃ": "hya", "ひゅ": "hyu", "ひょ": "hyo", "みゃ": "mya", "みゅ": "myu", "みょ": "myo", "りゃ": "rya", "りゅ": "ryu", "りょ": "ryo", "ぎゃ": "gya", "ぎゅ": "gyu", "ぎょ": "gyo", "じゃ": "ja", "じゅ": "ju", "じょ": "jo", "びゃ": "bya", "びゅ": "byu", "びょ": "byo", "ぴゃ": "pya", "ぴゅ": "pyu", "ぴょ": "pyo", "じぇ": "jie", "ちぇ": "chie", "てぃ": "tei", "でぃ": "dei", "でゅ": "deyu", "ふぁ": "fua", "ふぃ": "fui", "ふぇ": "fue", "ふぉ": "fuo", "ゔぁ": "bua", "ゔぃ": "bui", "ゔぇ": "bue", "ゔぉ": "buo" };
+const objTwoChars_K = { "キャ": "kya", "キュ": "kyu", "キョ": "kyo", "シャ": "sha", "シュ": "shu", "ショ": "sho", "チャ": "cha", "チュ": "chu", "チョ": "cho", "ニャ": "nya", "ニュ": "nyu", "ニョ": "nyo", "ヒャ": "hya", "ヒュ": "hyu", "ヒョ": "hyo", "ミャ": "mya", "ミュ": "myu", "ミョ": "myo", "リャ": "rya", "リュ": "ryu", "リョ": "ryo", "ギャ": "gya", "ギュ": "gyu", "ギョ": "gyo", "ジャ": "ja", "ジュ": "ju", "ジョ": "jo", "ビャ": "bya", "ビュ": "byu", "ビョ": "byo", "ピャ": "pya", "ピュ": "pyu", "ピョ": "pyo", "ジェ": "jie", "チェ": "chie", "ティ": "tei", "ディ": "dei", "デュ": "deyu", "ファ": "fua", "フィ": "fui", "フェ": "fue", "フォ": "fuo", "ヴァ": "bua", "ヴィ": "bui", "ヴェ": "bue", "ヴォ": "buo" };
+const objOneChar = { "あ": "a", "い": "i", "う": "u", "え": "e", "お": "o", "か": "ka", "き": "ki", "く": "ku", "け": "ke", "こ": "ko", "さ": "sa", "し": "shi", "す": "su", "せ": "se", "そ": "so", "た": "ta", "ち": "chi", "つ": "tsu", "て": "te", "と": "to", "な": "na", "に": "ni", "ぬ": "nu", "ね": "ne", "の": "no", "は": "ha", "ひ": "hi", "ふ": "fu", "へ": "he", "ほ": "ho", "ま": "ma", "み": "mi", "む": "mu", "め": "me", "も": "mo", "や": "ya", "ゆ": "yu", "よ": "yo", "ら": "ra", "り": "ri", "る": "ru", "れ": "re", "ろ": "ro", "わ": "wa", "ゐ ": "i", "ゑ": "e", "を": "o", "が": "ga", "ぎ": "gi", "ぐ": "gu", "げ": "ge", "ご": "go", "ざ": "za", "じ": "ji", "ず": "zu", "ぜ": "ze", "ぞ": "zo", "だ": "da", "ぢ": "ji", "づ": "zu", "で": "de", "ど": "do", "ば": "ba", "び": "bi", "ぶ": "bu", "べ": "be", "ぼ": "bo", "ぱ": "pa", "ぴ": "pi", "ぷ": "pu", "ぺ": "pe", "ぽ": "po", "ゔ": "bu", "ー": "" };
+const objOneChar_K = { "ア": "a", "イ": "i", "ウ": "u", "エ": "e", "オ": "o", "カ": "ka", "キ": "ki", "ク": "ku", "ケ": "ke", "コ": "ko", "サ": "sa", "シ": "shi", "ス": "su", "セ": "se", "ソ": "so", "タ": "ta", "チ": "chi", "ツ": "tsu", "テ": "te", "ト": "to", "ナ": "na", "ニ": "ni", "ヌ": "nu", "ネ": "ne", "ノ": "no", "ハ": "ha", "ヒ": "hi", "フ": "fu", "ヘ": "he", "ホ": "ho", "マ": "ma", "ミ": "mi", "ム": "mu", "メ": "me", "モ": "mo", "ヤ": "ya", "ユ": "yu", "ヨ": "yo", "ラ": "ra", "リ": "ri", "ル": "ru", "レ": "re", "ロ": "ro", "ワ": "wa", "ヰ ": "i", "ヱ": "e", "ヲ": "o", "ガ": "ga", "ギ": "gi", "グ": "gu", "ゲ": "ge", "ゴ": "go", "ザ": "za", "ジ": "ji", "ズ": "zu", "ゼ": "ze", "ゾ": "zo", "ダ": "da", "ヂ": "ji", "ヅ": "zu", "デ": "de", "ド": "do", "バ": "ba", "ビ": "bi", "ブ": "bu", "ベ": "be", "ボ": "bo", "パ": "pa", "ピ": "pi", "プ": "pu", "ペ": "pe", "ポ": "po", "ヴ": "bu", "ー": "" };
+const objM = { "んb": "mb", "んm": "mm", "んp": "mp" };
+const objM_K = { "ンb": "mb", "ンm": "mm", "ンp": "mp" };
+const objN = { "ん": "n" };
+const objN_K = { "ン": "n" };
+const objLongSound = { "oo": "o", "ou": "o", "uu": "u" };
+
+const MSG_PROMPT = "Please type or paste the sentences by [Hiragana] or [Katakana] here.";
 
 // 親：<Parent />の定義
 class Parent extends React.Component {
@@ -13,34 +24,105 @@ class Parent extends React.Component {
         super(props);
         this.state = {
             textVal: "",
-            children: [],
+            prompt: MSG_PROMPT,
         };
         this.setStateTextVal = this.setStateTextVal.bind(this);
     }
 
+
     // State(textVal)を変更
     setStateTextVal(textVal) {
-        this.setState({ textVal: textVal, });
+
+        if (prompt === "" && prompt != MSG_PROMPT) {
+
+            this.setState({
+                textVal: "",
+                prompt: MSG_PROMPT,
+            });
+
+        } else {
+            let textVal_r = textVal;
+
+            textVal_r = this.convertChars(textVal_r, objTwoChars_K);
+            textVal_r = this.convertChars(textVal_r, objTwoChars);
+
+            textVal_r = this.convertChars(textVal_r, objOneChar);
+            textVal_r = this.convertChars(textVal_r, objOneChar_K);
+
+            textVal_r = this.convertChars(textVal_r, objM);
+            textVal_r = this.convertChars(textVal_r, objM_K);
+
+            textVal_r = this.convertChars(textVal_r, objN);
+            textVal_r = this.convertChars(textVal_r, objN_K);
+
+            textVal_r = this.convertSmallTsu(textVal_r);
+
+            textVal_r = this.convertChars(textVal_r, objLongSound);
+
+            this.setState({
+                textVal: textVal_r,
+                prompt: textVal,
+            });
+        }
     }
 
-    // State(children)を変更
-    setStateChildren(textVal) {
-        var textVals = this.state.children.concat(textVal);
-        this.setState({ children: textVals, });
+    convertChars(text, obj) {
+        for (let key in obj) {
+            let arrText = text.split(key);
+            text = arrText.join(obj[key]);
+        }
+        return text;
     }
+
+    convertSmallTsu(text) {
+        text = this.convertChars(text, { "っch": "tch", "ッch": "tch" });
+
+        let arrText = text.split("");
+        for (let index in arrText) {
+            if (arrText[index] === "っ" || arrText[index] === "ッ") {
+                arrText[index] = arrText[Number(index) + 1] || "";
+            }
+        }
+        return arrText.join("");
+    }
+
 
     // <Parent />の表示
     // ここで子となる<ChildInput />と<Child />を記述
     render() {
         return (
-            <div>
-                <p>入力してEnterキーを押す</p>
-                <ChildInput
-                    onChange={(e) => { this.setStateTextVal(e) }}
-                    onSave={(e) => { this.setStateChildren(e) }}
-                />
-                <Child textVal={this.state.textVal} children={this.state.children} />
-            </div>
+            <center>
+                <h1>
+                    <b>Romaji<span className='hidden-xs'> </span><span className='visible-xs'><br /></span>Converter</b>
+                </h1>
+                <br />
+                <span className="redChar">※ Please also check the result by yourself.</span>
+                <table>
+                    <tbody>
+                        <tr>
+                            <th>
+                                <center>Hiragana<br />or<br />Katakana</center>
+                            </th>
+                            <th>
+                                <center>Romaji<br />（Hepburn-style）</center>
+                            </th>
+                        </tr>
+                        <tr>
+                            <td className="row">
+                                <ChildInput
+                                    prompt={this.state.prompt}
+                                    onChange={(e) => { this.setStateTextVal(e) }}
+                                />
+                            </td>
+                            <td className="tdOutput">
+                                <Child
+                                    textVal={this.state.textVal}
+                                />
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </center>
         );
     }
 };
@@ -53,40 +135,35 @@ class ChildInput extends React.Component {
 
     _onKeyDown(e) {
         if (e.keyCode === 13) { // Enterキー
-            this.props.onSave(e.target.value);
-            e.target.value = "";
+            //this.props.onSave(e.target.value);
+            //e.target.value = "";
         }
     }
 
     // <ChildInput />の表示
     render() {
         return (
-            <input
-                type="text"
-                onChange={(e) => { this._onChange(e) }}
-                onKeyDown={(e) => { this._onKeyDown(e) }} />
+            <center className="t-area-center">
+                <textarea
+                    id="input"
+                    onChange={(e) => { this._onChange(e) }}
+                    onKeyDown={(e) => { this._onKeyDown(e) }}
+                    value={this.props.prompt}
+                />
+            </center>
         );
     }
 };
+
 
 // 子2：<Child />の定義（※props経由で親を参照できる）
 class Child extends React.Component {
     // <Child />の表示
     render() {
-        var key = 0;
-        var textVals = this.props.children.map(function (textVal) {
-            // 時間が同じ。つまり、キーが押されるごとに、まとめて再描画されていることに注目
-            //（サーバーサイドっぽいと言われる所以）
-            var date = new Date().toString();
-            return <li key={key++}>{key}.{textVal}({date})</li>;
+        var lines = this.props.textVal.split('\n').map(function (line, index) {
+            return <p key={index} className="line-wrap">{line}<br /></p>;
         });
-
-        return (
-            <div>
-                <p>{this.props.textVal}</p>
-                <ul>{textVals}</ul>
-            </div>
-        );
+        return <div id="output" className="lines">{lines}</div>;
     }
 };
 
