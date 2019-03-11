7705be1abe97f3aae77d11a7ba96b8406edcdd24
Kosuke Zaizen <kosukezaizen@yahoo.co.jp>
2019-03-11 12:21:16 +0900

    add Romaji Converter

--- Z-Apps/ClientApp/src/components/RomajiConverter.js
+++ Z-Apps/ClientApp/src/components/RomajiConverter.js
@@ -5,155 +5,93 @@ import { actionCreators } from '../store/Counter';
 import '../css/RomajiConverter.css';
 
 
-function Square(props) {
-    return (
-        <button
-            className="square"
-            onClick={props.onClick}
-        >
-            {props.value}
-        </button>
-    );
-}
-
-
-class Board extends React.Component {
-
-    renderSquare(i) {
-        return (
-            <Square
-                value={this.props.squares[i]}
-                onClick={() => this.props.onClick(i)}
-            />
-        )
-    }
-
-    render() {
-        return (
-            <div>
-                <div className="board-row">
-                    {this.renderSquare(0)}
-                    {this.renderSquare(1)}
-                    {this.renderSquare(2)}
-                </div>
-                <div className="board-row">
-                    {this.renderSquare(3)}
-                    {this.renderSquare(4)}
-                    {this.renderSquare(5)}
-                </div>
-                <div className="board-row">
-                    {this.renderSquare(6)}
-                    {this.renderSquare(7)}
-                    {this.renderSquare(8)}
-                </div>
-            </div>
-        );
-    }
-}
-
-
-class Game extends React.Component {
-
+// 親：<Parent />の定義
+class Parent extends React.Component {
+    // State（※状態は親が管理）
+    // この値はブラウザを閉じたり、リロードするまでは保持される
     constructor(props) {
         super(props);
         this.state = {
-            history: [{
-                squares: Array(9).fill(null),
-            }],
-            stepNumber: 0,
-            xIsNext: true,
+            textVal: "",
+            children: [],
         };
+        this.setStateTextVal = this.setStateTextVal.bind(this);
     }
 
-    handleClick(i) {
-        const history = this.state.history.slice(0, this.state.stepNumber + 1);
-        const current = history[history.length - 1];
-        const squares = current.squares.slice();
-
-        if (calculateWinner(squares) || squares[i]) {
-            return;
-        }
-
-        squares[i] = this.state.xIsNext ? 'X' : 'O';
-        this.setState({
-            history: history.concat([{
-                squares: squares,
-            }]),
-            stepNumber: history.length,
-            xIsNext: !this.state.xIsNext,
-        });
+    // State(textVal)を変更
+    setStateTextVal(textVal) {
+        this.setState({ textVal: textVal, });
     }
 
-    jumpTo(step) {
-        this.setState({
-            stepNumber: step,
-            xIsNext: (step % 2) === 0,
-        });
+    // State(children)を変更
+    setStateChildren(textVal) {
+        var textVals = this.state.children.concat(textVal);
+        this.setState({ children: textVals, });
     }
 
+    // <Parent />の表示
+    // ここで子となる<ChildInput />と<Child />を記述
     render() {
-        const history = this.state.history;
-        const current = history[this.state.stepNumber];
-        const winner = calculateWinner(current.squares);
-
-        const moves = history.map((step, move) => {
-            const desc = move ?
-                'Go to move #' + move :
-                'Go to game start';
-            return (
-                <li key={move}>
-                    <button onClick={() => this.jumpTo(move)}>{desc}</button>
-                </li>
-            );
-        });
-
-        let status;
-        if (winner) {
-            status = 'Winner: ' + winner;
-        } else {
-            status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
-        }
-
         return (
-            <div className="game" >
-                <div className="game-board">
-                    <Board
-                        squares={current.squares}
-                        onClick={(i) => this.handleClick(i)}
-                    />
-                </div>
-                <div className="game-info">
-                    <div>{status}</div>
-                    <ol>{moves}</ol>
-                </div>
+            <div>
+                <p>入力してEnterキーを押す</p>
+                <ChildInput
+                    onChange={(e) => { this.setStateTextVal(e) }}
+                    onSave={(e) => { this.setStateChildren(e) }}
+                />
+                <Child textVal={this.state.textVal} children={this.state.children} />
             </div>
         );
     }
-}
+};
 
+// 子1：<ChildInput />の定義（※props経由で親を参照できる）
+class ChildInput extends React.Component {
+    _onChange(e) {
+        this.props.onChange(e.target.value);
+    }
 
-function calculateWinner(squares) {
-    const lines = [
-        [0, 1, 2],
-        [3, 4, 5],
-        [6, 7, 8],
-        [0, 3, 6],
-        [1, 4, 7],
-        [2, 5, 8],
-        [0, 4, 8],
-        [2, 4, 6],
-    ];
-    for (let i = 0; i < lines.length; i++) {
-        const [a, b, c] = lines[i];
-        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-            return squares[a];
+    _onKeyDown(e) {
+        if (e.keyCode === 13) { // Enterキー
+            this.props.onSave(e.target.value);
+            e.target.value = "";
         }
     }
-    return null;
-}
+
+    // <ChildInput />の表示
+    render() {
+        return (
+            <input
+                type="text"
+                onChange={(e) => { this._onChange(e) }}
+                onKeyDown={(e) => { this._onKeyDown(e) }} />
+        );
+    }
+};
+
+// 子2：<Child />の定義（※props経由で親を参照できる）
+class Child extends React.Component {
+    // <Child />の表示
+    render() {
+        var key = 0;
+        var textVals = this.props.children.map(function (textVal) {
+            // 時間が同じ。つまり、キーが押されるごとに、まとめて再描画されていることに注目
+            //（サーバーサイドっぽいと言われる所以）
+            var date = new Date().toString();
+            return <li key={key++}>{key}.{textVal}({date})</li>;
+        });
+
+        return (
+            <div>
+                <p>{this.props.textVal}</p>
+                <ul>{textVals}</ul>
+            </div>
+        );
+    }
+};
 
 
 export default connect(
     state => state.counter,
     dispatch => bindActionCreators(actionCreators, dispatch)
-)(Game);
+)(Parent);
