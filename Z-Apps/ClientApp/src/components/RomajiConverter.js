49530b9d9460e5f2bfc944977b238246efdfb8f7
Kosuke Zaizen <kosukezaizen@yahoo.co.jp>
2019-03-14 10:57:18 +0900

    make const for each page

--- Z-Apps/ClientApp/src/components/RomajiConverter.js
+++ Z-Apps/ClientApp/src/components/RomajiConverter.js
@@ -4,27 +4,7 @@ import { connect } from 'react-redux';
 import { actionCreators } from '../store/Counter';
 import '../css/RomajiConverter.css';
 
-const objTwoChars = { "きゃ": "kya", "きゅ": "kyu", "きょ": "kyo", "しゃ": "sha", "しゅ": "shu", "しょ": "sho", "ちゃ": "cha", "ちゅ": "chu", "ちょ": "cho", "にゃ": "nya", "にゅ": "nyu", "にょ": "nyo", "ひゃ": "hya", "ひゅ": "hyu", "ひょ": "hyo", "みゃ": "mya", "みゅ": "myu", "みょ": "myo", "りゃ": "rya", "りゅ": "ryu", "りょ": "ryo", "ぎゃ": "gya", "ぎゅ": "gyu", "ぎょ": "gyo", "じゃ": "ja", "じゅ": "ju", "じょ": "jo", "びゃ": "bya", "びゅ": "byu", "びょ": "byo", "ぴゃ": "pya", "ぴゅ": "pyu", "ぴょ": "pyo", "じぇ": "jie", "ちぇ": "chie", "てぃ": "tei", "でぃ": "dei", "でゅ": "deyu", "ふぁ": "fua", "ふぃ": "fui", "ふぇ": "fue", "ふぉ": "fuo", "ゔぁ": "bua", "ゔぃ": "bui", "ゔぇ": "bue", "ゔぉ": "buo" };
-const objTwoChars_K = { "キャ": "kya", "キュ": "kyu", "キョ": "kyo", "シャ": "sha", "シュ": "shu", "ショ": "sho", "チャ": "cha", "チュ": "chu", "チョ": "cho", "ニャ": "nya", "ニュ": "nyu", "ニョ": "nyo", "ヒャ": "hya", "ヒュ": "hyu", "ヒョ": "hyo", "ミャ": "mya", "ミュ": "myu", "ミョ": "myo", "リャ": "rya", "リュ": "ryu", "リョ": "ryo", "ギャ": "gya", "ギュ": "gyu", "ギョ": "gyo", "ジャ": "ja", "ジュ": "ju", "ジョ": "jo", "ビャ": "bya", "ビュ": "byu", "ビョ": "byo", "ピャ": "pya", "ピュ": "pyu", "ピョ": "pyo", "ジェ": "jie", "チェ": "chie", "ティ": "tei", "ディ": "dei", "デュ": "deyu", "ファ": "fua", "フィ": "fui", "フェ": "fue", "フォ": "fuo", "ヴァ": "bua", "ヴィ": "bui", "ヴェ": "bue", "ヴォ": "buo" };
-const objOneChar = { "あ": "a", "い": "i", "う": "u", "え": "e", "お": "o", "か": "ka", "き": "ki", "く": "ku", "け": "ke", "こ": "ko", "さ": "sa", "し": "shi", "す": "su", "せ": "se", "そ": "so", "た": "ta", "ち": "chi", "つ": "tsu", "て": "te", "と": "to", "な": "na", "に": "ni", "ぬ": "nu", "ね": "ne", "の": "no", "は": "ha", "ひ": "hi", "ふ": "fu", "へ": "he", "ほ": "ho", "ま": "ma", "み": "mi", "む": "mu", "め": "me", "も": "mo", "や": "ya", "ゆ": "yu", "よ": "yo", "ら": "ra", "り": "ri", "る": "ru", "れ": "re", "ろ": "ro", "わ": "wa", "ゐ ": "i", "ゑ": "e", "を": "o", "が": "ga", "ぎ": "gi", "ぐ": "gu", "げ": "ge", "ご": "go", "ざ": "za", "じ": "ji", "ず": "zu", "ぜ": "ze", "ぞ": "zo", "だ": "da", "ぢ": "ji", "づ": "zu", "で": "de", "ど": "do", "ば": "ba", "び": "bi", "ぶ": "bu", "べ": "be", "ぼ": "bo", "ぱ": "pa", "ぴ": "pi", "ぷ": "pu", "ぺ": "pe", "ぽ": "po", "ゔ": "bu", "ー": "" };
-const objOneChar_K = { "ア": "a", "イ": "i", "ウ": "u", "エ": "e", "オ": "o", "カ": "ka", "キ": "ki", "ク": "ku", "ケ": "ke", "コ": "ko", "サ": "sa", "シ": "shi", "ス": "su", "セ": "se", "ソ": "so", "タ": "ta", "チ": "chi", "ツ": "tsu", "テ": "te", "ト": "to", "ナ": "na", "ニ": "ni", "ヌ": "nu", "ネ": "ne", "ノ": "no", "ハ": "ha", "ヒ": "hi", "フ": "fu", "ヘ": "he", "ホ": "ho", "マ": "ma", "ミ": "mi", "ム": "mu", "メ": "me", "モ": "mo", "ヤ": "ya", "ユ": "yu", "ヨ": "yo", "ラ": "ra", "リ": "ri", "ル": "ru", "レ": "re", "ロ": "ro", "ワ": "wa", "ヰ ": "i", "ヱ": "e", "ヲ": "o", "ガ": "ga", "ギ": "gi", "グ": "gu", "ゲ": "ge", "ゴ": "go", "ザ": "za", "ジ": "ji", "ズ": "zu", "ゼ": "ze", "ゾ": "zo", "ダ": "da", "ヂ": "ji", "ヅ": "zu", "デ": "de", "ド": "do", "バ": "ba", "ビ": "bi", "ブ": "bu", "ベ": "be", "ボ": "bo", "パ": "pa", "ピ": "pi", "プ": "pu", "ペ": "pe", "ポ": "po", "ヴ": "bu", "ー": "" };
-const objM = { "んb": "mb", "んm": "mm", "んp": "mp" };
-const objM_K = { "ンb": "mb", "ンm": "mm", "ンp": "mp" };
-const objN = { "ん": "n" };
-const objN_K = { "ン": "n" };
-const objLongSound = { "oo": "o", "ou": "o", "uu": "u" };
-const objChangeLine = { "<br />": "\n", "<br/>": "\n", "<br>": "\n", '<p class="line-wrap">': "", "</p>": "" };
-
-const MSG_PROMPT = "Please type or paste the sentences of [Hiragana] or [Katakana] here.";
-const MSG_NO_COPY_TARGET = "There is no Romaji characters to copy!\r\nPlease input Hiragana or Katakana!";
-const MSG_COPY_DONE = "Copy was done!\r\nYou can paste it anywhere.";
-const MSG_COPY_ERR = "Sorry!\r\nYou can not use the copy function with this web browser.\r\nPlease copy it manually.";
-
-const BTN_LABEL = "Click here to copy the output Romaji!"
-
-const DISABLED_BUTTON = "btn btn-outline-info btn-lg btn-block";
-
-let ioArea = [];
+let objCONST = {};
 
 // 親：<Parent />の定義
 class Parent extends React.Component {
@@ -33,9 +13,33 @@ class Parent extends React.Component {
     // この値はブラウザを閉じたり、リロードするまでは保持される
     constructor(props) {
         super(props);
+        objCONST = {
+            objTwoChars: { "きゃ": "kya", "きゅ": "kyu", "きょ": "kyo", "しゃ": "sha", "しゅ": "shu", "しょ": "sho", "ちゃ": "cha", "ちゅ": "chu", "ちょ": "cho", "にゃ": "nya", "にゅ": "nyu", "にょ": "nyo", "ひゃ": "hya", "ひゅ": "hyu", "ひょ": "hyo", "みゃ": "mya", "みゅ": "myu", "みょ": "myo", "りゃ": "rya", "りゅ": "ryu", "りょ": "ryo", "ぎゃ": "gya", "ぎゅ": "gyu", "ぎょ": "gyo", "じゃ": "ja", "じゅ": "ju", "じょ": "jo", "びゃ": "bya", "びゅ": "byu", "びょ": "byo", "ぴゃ": "pya", "ぴゅ": "pyu", "ぴょ": "pyo", "じぇ": "jie", "ちぇ": "chie", "てぃ": "tei", "でぃ": "dei", "でゅ": "deyu", "ふぁ": "fua", "ふぃ": "fui", "ふぇ": "fue", "ふぉ": "fuo", "ゔぁ": "bua", "ゔぃ": "bui", "ゔぇ": "bue", "ゔぉ": "buo" },
+            objTwoChars_K: { "キャ": "kya", "キュ": "kyu", "キョ": "kyo", "シャ": "sha", "シュ": "shu", "ショ": "sho", "チャ": "cha", "チュ": "chu", "チョ": "cho", "ニャ": "nya", "ニュ": "nyu", "ニョ": "nyo", "ヒャ": "hya", "ヒュ": "hyu", "ヒョ": "hyo", "ミャ": "mya", "ミュ": "myu", "ミョ": "myo", "リャ": "rya", "リュ": "ryu", "リョ": "ryo", "ギャ": "gya", "ギュ": "gyu", "ギョ": "gyo", "ジャ": "ja", "ジュ": "ju", "ジョ": "jo", "ビャ": "bya", "ビュ": "byu", "ビョ": "byo", "ピャ": "pya", "ピュ": "pyu", "ピョ": "pyo", "ジェ": "jie", "チェ": "chie", "ティ": "tei", "ディ": "dei", "デュ": "deyu", "ファ": "fua", "フィ": "fui", "フェ": "fue", "フォ": "fuo", "ヴァ": "bua", "ヴィ": "bui", "ヴェ": "bue", "ヴォ": "buo" },
+            objOneChar: { "あ": "a", "い": "i", "う": "u", "え": "e", "お": "o", "か": "ka", "き": "ki", "く": "ku", "け": "ke", "こ": "ko", "さ": "sa", "し": "shi", "す": "su", "せ": "se", "そ": "so", "た": "ta", "ち": "chi", "つ": "tsu", "て": "te", "と": "to", "な": "na", "に": "ni", "ぬ": "nu", "ね": "ne", "の": "no", "は": "ha", "ひ": "hi", "ふ": "fu", "へ": "he", "ほ": "ho", "ま": "ma", "み": "mi", "む": "mu", "め": "me", "も": "mo", "や": "ya", "ゆ": "yu", "よ": "yo", "ら": "ra", "り": "ri", "る": "ru", "れ": "re", "ろ": "ro", "わ": "wa", "ゐ ": "i", "ゑ": "e", "を": "o", "が": "ga", "ぎ": "gi", "ぐ": "gu", "げ": "ge", "ご": "go", "ざ": "za", "じ": "ji", "ず": "zu", "ぜ": "ze", "ぞ": "zo", "だ": "da", "ぢ": "ji", "づ": "zu", "で": "de", "ど": "do", "ば": "ba", "び": "bi", "ぶ": "bu", "べ": "be", "ぼ": "bo", "ぱ": "pa", "ぴ": "pi", "ぷ": "pu", "ぺ": "pe", "ぽ": "po", "ゔ": "bu", "ー": "" },
+            objOneChar_K: { "ア": "a", "イ": "i", "ウ": "u", "エ": "e", "オ": "o", "カ": "ka", "キ": "ki", "ク": "ku", "ケ": "ke", "コ": "ko", "サ": "sa", "シ": "shi", "ス": "su", "セ": "se", "ソ": "so", "タ": "ta", "チ": "chi", "ツ": "tsu", "テ": "te", "ト": "to", "ナ": "na", "ニ": "ni", "ヌ": "nu", "ネ": "ne", "ノ": "no", "ハ": "ha", "ヒ": "hi", "フ": "fu", "ヘ": "he", "ホ": "ho", "マ": "ma", "ミ": "mi", "ム": "mu", "メ": "me", "モ": "mo", "ヤ": "ya", "ユ": "yu", "ヨ": "yo", "ラ": "ra", "リ": "ri", "ル": "ru", "レ": "re", "ロ": "ro", "ワ": "wa", "ヰ ": "i", "ヱ": "e", "ヲ": "o", "ガ": "ga", "ギ": "gi", "グ": "gu", "ゲ": "ge", "ゴ": "go", "ザ": "za", "ジ": "ji", "ズ": "zu", "ゼ": "ze", "ゾ": "zo", "ダ": "da", "ヂ": "ji", "ヅ": "zu", "デ": "de", "ド": "do", "バ": "ba", "ビ": "bi", "ブ": "bu", "ベ": "be", "ボ": "bo", "パ": "pa", "ピ": "pi", "プ": "pu", "ペ": "pe", "ポ": "po", "ヴ": "bu", "ー": "" },
+            objM: { "んb": "mb", "んm": "mm", "んp": "mp" },
+            objM_K: { "ンb": "mb", "ンm": "mm", "ンp": "mp" },
+            objN: { "ん": "n" },
+            objN_K: { "ン": "n" },
+            objLongSound: { "oo": "o", "ou": "o", "uu": "u" },
+            objChangeLine: { "<br />": "\n", "<br/>": "\n", "<br>": "\n", '<p class="line-wrap">': "", "</p>": "" },
+
+            MSG_PROMPT: "Please type or paste the sentences of [Hiragana] or [Katakana] here.",
+            MSG_NO_COPY_TARGET: "There is no Romaji characters to copy!\r\nPlease input Hiragana or Katakana!",
+            MSG_COPY_DONE: "Copy was done!\r\nYou can paste it anywhere.",
+            MSG_COPY_ERR: "Sorry!\r\nYou can not use the copy function with this web browser.\r\nPlease copy it manually.",
+
+            BTN_LABEL: "Click here to copy the output Romaji!",
+
+            DISABLED_BUTTON: "btn btn-outline-info btn-lg btn-block",
+
+            ioArea: []
+        };
+
         this.state = {
             textVal: "",
-            prompt: MSG_PROMPT,
+            prompt: objCONST.MSG_PROMPT,
             inputColor: "redChar",
         };
         this.setStateTextVal = this.setStateTextVal.bind(this);
@@ -44,7 +48,7 @@ class Parent extends React.Component {
 
 
     initText() {
-        if (this.state.prompt === MSG_PROMPT) {
+        if (this.state.prompt === objCONST.MSG_PROMPT) {
             this.setState({
                 prompt: "",
                 inputColor: "blackChar",
@@ -58,21 +62,21 @@ class Parent extends React.Component {
 
         let textVal_r = textVal;
 
-        textVal_r = convertChars(textVal_r, objTwoChars_K);
-        textVal_r = convertChars(textVal_r, objTwoChars);
+        textVal_r = convertChars(textVal_r, objCONST.objTwoChars_K);
+        textVal_r = convertChars(textVal_r, objCONST.objTwoChars);
 
-        textVal_r = convertChars(textVal_r, objOneChar);
-        textVal_r = convertChars(textVal_r, objOneChar_K);
+        textVal_r = convertChars(textVal_r, objCONST.objOneChar);
+        textVal_r = convertChars(textVal_r, objCONST.objOneChar_K);
 
-        textVal_r = convertChars(textVal_r, objM);
-        textVal_r = convertChars(textVal_r, objM_K);
+        textVal_r = convertChars(textVal_r, objCONST.objM);
+        textVal_r = convertChars(textVal_r, objCONST.objM_K);
 
-        textVal_r = convertChars(textVal_r, objN);
-        textVal_r = convertChars(textVal_r, objN_K);
+        textVal_r = convertChars(textVal_r, objCONST.objN);
+        textVal_r = convertChars(textVal_r, objCONST.objN_K);
 
         textVal_r = this.convertSmallTsu(textVal_r);
 
-        textVal_r = convertChars(textVal_r, objLongSound);
+        textVal_r = convertChars(textVal_r, objCONST.objLongSound);
 
         this.setState({
             textVal: textVal_r,
@@ -94,20 +98,20 @@ class Parent extends React.Component {
 
     onScrollInput() {
         getIoElement();
-        ioArea[1].scrollTop = ioArea[0].scrollTop;
+        objCONST.ioArea[1].scrollTop = objCONST.ioArea[0].scrollTop;
     }
 
     onClickCopy() {
         let strTarget = getCopyTarget();
 
         if (strTarget.trim() === "") {
-            alert(MSG_NO_COPY_TARGET);
+            alert(objCONST.MSG_NO_COPY_TARGET);
         } else {
             if (execCopy(strTarget)) {
-                alert(MSG_COPY_DONE);
+                alert(objCONST.MSG_COPY_DONE);
             }
             else {
-                alert(MSG_COPY_ERR);
+                alert(objCONST.MSG_COPY_ERR);
             }
         }
     }
@@ -151,7 +155,7 @@ class Parent extends React.Component {
                         </tr>
                     </tbody>
                 </table>
-                <button id="btnCopy" onClick={this.onClickCopy} className={DISABLED_BUTTON}>{BTN_LABEL}</button>
+                <button id="btnCopy" onClick={this.onClickCopy} className={objCONST.DISABLED_BUTTON}>{objCONST.BTN_LABEL}</button>
             </center>
         );
     }
@@ -209,9 +213,9 @@ class Child extends React.Component {
 };
 
 function getIoElement() {
-    if (ioArea.length < 2) {
-        ioArea[0] = document.getElementById("inputArea");
-        ioArea[1] = document.getElementById("outputArea");
+    if (objCONST.ioArea.length < 2) {
+        objCONST.ioArea[0] = document.getElementById("inputArea");
+        objCONST.ioArea[1] = document.getElementById("outputArea");
     }
 }
 
@@ -225,7 +229,7 @@ function convertChars(text, obj) {
 
 function getCopyTarget() {
     getIoElement();
-    return convertChars(ioArea[1].innerHTML, objChangeLine);
+    return convertChars(objCONST.ioArea[1].innerHTML, objCONST.objChangeLine);
 }
 
 function execCopy(string) {
