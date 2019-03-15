4589b6c1a3e6db27086246af4dc96b305bc55c8d
Kosuke Zaizen <kosukezaizen@yahoo.co.jp>
2019-03-15 19:30:52 +0900

    make IncorrectTable

--- Z-Apps/ClientApp/src/components/parts/KanaQuiz/KanaQuiz3.js
+++ Z-Apps/ClientApp/src/components/parts/KanaQuiz/KanaQuiz3.js
@@ -1,6 +1,45 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+class IncorrectTable extends React.Component {
+    /*
+    var htmlInc = "";
+    if (this.incorrectList) {
+    htmlInc = this.incorrectList ? "<h3>Characters you should remember:</h3>" : "";
+    }
+
+    htmlInc += "<h1><table style='font-size: 15pt;'>";
+    for (let key in this.incorrectList) {
+    htmlInc = htmlInc + "<tr><td>&nbsp;&nbsp;&nbsp;" + this.incorrectList[key] + "</td><td> :   </td><td>" + key + "</td>";
+    }
+    htmlInc = htmlInc + "</table></h1>";
+    */
+
+    render() {
+        let top = "";
+        if (Object.keys(this.props.incorrectList).length > 0) {
+            top = "Characters you should remember:";
+        }
+
+        let trList = [];
+        for (let key in this.props.incorrectList) {
+            trList.push(<tr key={key}><td>{this.props.incorrectList[key]}</td><td>:</td><td>{key}</td></tr>);
+        }
+
+        return (
+            <div>
+                {top}
+                <table>
+                    <tbody>
+                        {trList}
+                    </tbody>
+                </table>
+            </div>
+        );
+    }
+}
+
+
 export default class Quiz3 extends React.Component {
     render() {
         return (
@@ -11,9 +50,9 @@ export default class Quiz3 extends React.Component {
                     /
                     {this.props.maxChar}</h1>
                 <br />
-                {this.props.incorrectTable}
+                <IncorrectTable incorrectList={this.props.incorrectList} />
                 <br />
-                <button onClick={() => { this.props.changePage(1)}} type="button">Retry</button>
+                <button onClick={() => { this.props.changePage(1) }} type="button">Retry</button>
                 <br />
                 <br />
                 <div className="related-articles">
