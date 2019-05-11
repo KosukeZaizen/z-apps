f9a2b4b72dd82d9cf675979240ad15d4f721c835
Kosuke Zaizen <kosukezaizen@yahoo.co.jp>
2019-05-11 17:46:07 +0900

    making ninja game 3

---
+++ Z-Apps/ClientApp/src/components/NinjaGame3.js
@@ -0,0 +1,93 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { Page1 } from './parts/Ninja2/Page1';
+import { Page2 } from './parts/Ninja2/Page2';
+import '../css/NinjaGame2.css';
+
+
+class NinjaGame extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.consts = {
+        };
+        this.state = {
+            language: "English",
+            curPage: 1,
+            stage: 1,//デバッグ用（通常時1）★
+            //stage: 1,
+            ninja: {
+                size: 12,
+                speedX: 0,
+                speedY: 0,
+                posX: 145,
+                posY: 60,
+                readScroll: [],
+                boolLeft: true,
+            },
+        };
+        this.readElementScroll = [];
+    }
+
+    changePage(num, lang) {
+        this.setState({
+            curPage: num,
+            language: lang,
+        });
+    }
+
+    changeStage(num, ninja) {
+        this.readElementScroll = [];
+        this.setState({
+            stage: num,
+            ninja: ninja,
+            curPage: 2,
+        });
+    }
+
+    render() {
+        let style = {
+            position: "fixed",
+            top: 0,
+            left: 0,
+            width: "100%",
+            height: "100%",
+            backgroundColor: "black",
+            zIndex: 9999999,
+        };
+
+        return (
+            <center id="ninja-game" style={style}>
+                <Pages
+                    state={this.state}
+                    changePage={(i, lang) => { this.changePage(i, lang) }}
+                    changeStage={(i, j) => { this.changeStage(i, j) }}
+                    changeLanguage={() => { this.changeLanguage() }}
+                    readElementScroll={this.readElementScroll}
+                />
+            </center>
+        )
+    }
+};
+
+function Pages(props) {
+    if (props.state.curPage === 1) {
+        return (
+            <Page1
+                changePage={(i,lang) => { props.changePage(i,lang) }}
+            />
+        );
+    } else if (props.state.curPage === 2) {
+        return (
+            <Page2
+                changeStage={(i, j) => { props.changeStage(i, j) }}
+                ninja={props.state.ninja}
+                stage={props.state.stage}
+                readElementScroll={props.readElementScroll}
+                language={props.state.language}
+            />
+        );
+    }
+}
+
+export default connect()(NinjaGame);
---
+++ Z-Apps/ClientApp/src/components/parts/Ninja3/Page1.js
@@ -0,0 +1,72 @@
+import React from 'react';
+import logo from './img/logo.png';
+
+
+export default class Page1 extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.consts = {
+            BTN_START_CLASS: "btn btn-dark btn-lg btn-block",
+        };
+        this.state = {
+            topImage: true,
+        };
+    }
+
+    hideTopImage() {
+        this.setState({ topImage: false, });
+    }
+
+    render() {
+        return (
+            <div id="page1">
+                <span onClick={() => { this.hideTopImage() }}>
+                    <TopImage topImage={this.state.topImage} />
+                </span>
+                <br />
+                <br />
+                <h1 style={{ color: "white", }}>Which language do you prefer?</h1>
+                <br />
+                <span onClick={() => { this.props.changePage(2, "English") }}>
+                    <button
+                        className={this.consts.BTN_START_CLASS}
+                    >
+                        {"English"}
+                    </button>
+                </span>
+                <br />
+                <span onClick={() => { this.props.changePage(2, "Japanese") }}>
+                    <button
+                        className={this.consts.BTN_START_CLASS}
+                    >
+                        {"日本語"}
+                    </button>
+                </span>
+                <br />
+                <center style={{color:"white"}}>
+                    If you want to be a real Ninja, please check this:<br />
+                    <a
+                        href="https://www.lingual-ninja.com/2018/09/how-to-be-ninja.html"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        How to be a Ninja >>
+                        </a>
+                </center>
+                <br />
+                <br />
+            </div>
+        );
+    }
+}
+
+function TopImage(props) {
+    if (props.topImage) {
+        return <img width="100%" src={logo} alt="Shuriken Master" />;
+    } else {
+        return <span></span>;
+    }
+}
+
+export { Page1 };
