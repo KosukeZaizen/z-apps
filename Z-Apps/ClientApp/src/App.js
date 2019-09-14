cee20aa004e66d6360feb7de2bc0ef31e9742802
Kosuke Zaizen <kosukezaizen@yahoo.co.jp>
2019-09-14 13:18:53 +0900

    install google analytics

--- Z-Apps/ClientApp/src/App.js
+++ Z-Apps/ClientApp/src/App.js
@@ -2,6 +2,7 @@ import React, { Suspense, lazy } from 'react';
 import { Route, Switch, Redirect } from 'react-router';
 import CircularProgress from '@material-ui/core/CircularProgress';
 import Layout from './components/parts/Layout';
+import ReactGA from 'react-ga';
 
 const Home = lazy(() => import('./components/Home'));
 const Terms = lazy(() => import('./components/Terms'));
@@ -20,32 +21,42 @@ const GameOver = lazy(() => import('./components/GameOver'));
 const ColorPalette = lazy(() => import('./components/ColorPalette'));
 const NotFound = lazy(() => import('./components/404'));
 
-export default () => (
-    <Layout>
-        <Suspense fallback={<LoadingAnimation num={1} />}>
-            <Switch>
-                <Route exact path='/' component={Home} />
-                <Route path='/terms' component={Terms} />
-                <Route path='/developer' component={Developer} />
-                <Route path='/kanji-converter' component={KanjiConverter} />
-                <Route path='/romaji-converter' component={RomajiConverter} />
-                <Route path='/hiragana-quiz' component={HiraganaQuiz} />
-                <Route path='/katakana-quiz' component={KatakanaQuiz} />
-                <Route path='/stories' component={Stories} />
-                <Route path='/stories-setting' component={StoriesSetting} />
-                <Route path='/ninja' component={NinjaTop} />
-                <Route path='/ninja1' component={Ninja1} />
-                <Route path='/ninja2' component={Ninja2} />
-                <Route path='/ninja3' component={Ninja3} />
-                <Route path='/game-over' component={GameOver} />
-                <Route path='/color-code' component={ColorPalette} />
-                <Route path='/not-found' component={NotFound} />
-                <Route component={NotFoundRedirect} />
-            </Switch>
-        </Suspense>
-    </Layout>
+export default class App extends React.Component {
 
-);
+    componentDidMount() {
+        const { pathname } = window.location;
+        ReactGA.set({ page: pathname });
+        ReactGA.pageview(pathname);
+    }
+
+    render() {
+        return (
+            <Layout>
+                <Suspense fallback={<LoadingAnimation num={1} />}>
+                    <Switch>
+                        <Route exact path='/' component={Home} />
+                        <Route path='/terms' component={Terms} />
+                        <Route path='/developer' component={Developer} />
+                        <Route path='/kanji-converter' component={KanjiConverter} />
+                        <Route path='/romaji-converter' component={RomajiConverter} />
+                        <Route path='/hiragana-quiz' component={HiraganaQuiz} />
+                        <Route path='/katakana-quiz' component={KatakanaQuiz} />
+                        <Route path='/stories' component={Stories} />
+                        <Route path='/stories-setting' component={StoriesSetting} />
+                        <Route path='/ninja' component={NinjaTop} />
+                        <Route path='/ninja1' component={Ninja1} />
+                        <Route path='/ninja2' component={Ninja2} />
+                        <Route path='/ninja3' component={Ninja3} />
+                        <Route path='/game-over' component={GameOver} />
+                        <Route path='/color-code' component={ColorPalette} />
+                        <Route path='/not-found' component={NotFound} />
+                        <Route component={NotFoundRedirect} />
+                    </Switch>
+                </Suspense>
+            </Layout>
+        );
+    }
+}
 
 function NotFoundRedirect({ location }) {
     return (
--- Z-Apps/ClientApp/src/index.js
+++ Z-Apps/ClientApp/src/index.js
@@ -7,10 +7,18 @@ import { createBrowserHistory } from 'history';
 import configureStore from './store/configureStore';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import ReactGA from 'react-ga';
+
+ReactGA.initialize('UA-122613642-6');
 
 // Create browser history to use in the Redux store
 const baseUrl = document.getElementsByTagName('base')[0].getAttribute('href');
 const history = createBrowserHistory({ basename: baseUrl });
+
+history.listen(({ pathname }) => {
+    ReactGA.set({ page: pathname });
+    ReactGA.pageview(pathname);
+});
 
 // Get the application-wide store instance, prepopulating with state from the server where available.
 const initialState = window.initialReduxState;
