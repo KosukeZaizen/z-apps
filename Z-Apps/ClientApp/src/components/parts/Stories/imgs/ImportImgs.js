cdb7ef3b260af410d140bf07dc7f4e71e1962cb4
Kosuke Zaizen <kosukezaizen@yahoo.co.jp>
2019-10-21 21:18:59 +0900

    before releasing Kachi Kachi Yama

--- Z-Apps/ClientApp/src/components/parts/Stories/imgs/ImportImgs.js
+++ Z-Apps/ClientApp/src/components/parts/Stories/imgs/ImportImgs.js
@@ -1,11 +1,13 @@
-import Momotaro from './Momotaro/Momotaro.png';
-import Urashima_Taro from './Urashima-Taro/Urashima-Taro.png';
-import Issunboushi from './Issunboushi/Issunboushi.png';
-import Kaguya_Hime from './Kaguya-Hime/kaguya-hime.png';
+import Momotaro from './Momotaro.png';
+import Urashima_Taro from './Urashima-Taro.png';
+import Issunboushi from './Issunboushi.png';
+import Kaguya_Hime from './kaguya-hime.png';
+import Kasa_Jizo from './kasa-jizo.png';
 
 export default {
     Momotaro,
     Urashima_Taro,
     Issunboushi,
     Kaguya_Hime,
+    Kasa_Jizo,
 }
