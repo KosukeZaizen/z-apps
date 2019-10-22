ef53b5dde93f937145b6d16f1e584f31212b9e4c
Kosuke Zaizen <kosukezaizen@yahoo.co.jp>
2019-10-22 20:16:55 +0900

    add Yuki Onna picture

--- Z-Apps/ClientApp/src/components/parts/Stories/imgs/ImportImgs.js
+++ Z-Apps/ClientApp/src/components/parts/Stories/imgs/ImportImgs.js
@@ -3,6 +3,8 @@ import Urashima_Taro from './Urashima-Taro.png';
 import Issunboushi from './Issunboushi.png';
 import Kaguya_Hime from './kaguya-hime.png';
 import Kasa_Jizo from './kasa-jizo.png';
+import Kachi_Kachi_Yama from './Kachi-Kachi-Yama.jpg';
+import Yuki_Onna from './Yuki-Onna.png';
 
 export default {
     Momotaro,
@@ -10,4 +12,6 @@ export default {
     Issunboushi,
     Kaguya_Hime,
     Kasa_Jizo,
+    Kachi_Kachi_Yama,
+    Yuki_Onna,
 }
