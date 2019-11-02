4ca19599aca4a0a842f44c2d1b494e21e8ca3dab
Kosuke Zaizen <kosukezaizen@yahoo.co.jp>
2019-11-02 22:27:03 +0900

    add Kintaro

--- Z-Apps/ClientApp/src/components/parts/Stories/imgs/ImportImgs.js
+++ Z-Apps/ClientApp/src/components/parts/Stories/imgs/ImportImgs.js
@@ -7,7 +7,7 @@ import Kachi_Kachi_Yama from './Kachi-Kachi-Yama.jpg';
 import Yuki_Onna from './Yuki-Onna.png';
 import Hanasaka_Jiisan from './hanasaka-jiisan.png';
 import Orihime_and_Hikoboshi_Tanabata from './Orihime-and-Hikoboshi-Tanabata.jpg';
-
+import Kintaro_Sakata_Kintoki from './Kintaro.png';
 
 export default {
     Momotaro,
@@ -19,4 +19,5 @@ export default {
     Yuki_Onna,
     Hanasaka_Jiisan,
     Orihime_and_Hikoboshi_Tanabata,
+    Kintaro_Sakata_Kintoki,
 }
