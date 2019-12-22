292fb4aafe88aa50697dc9382bae0f34ba0fb0da
Kosuke Zaizen <kosukezaizen@yahoo.co.jp>
2019-12-22 22:21:32 +0900

    modify facebook component height

--- Z-Apps/ClientApp/src/components/parts/FaceBook.js
+++ Z-Apps/ClientApp/src/components/parts/FaceBook.js
@@ -3,15 +3,21 @@ import React from 'react';
 export default class FB extends React.Component {
     render() {
         const innerWidth = parseInt(window.innerWidth, 10);
-        const width = (innerWidth > 350) ? 350 : 300;
+        let width;
+        if (innerWidth > 350) {
+            width = 350;
+        } else {
+            width = 300;
+        }
+        const height = 185;
 
         return (
             <center>
                 <iframe
                     title="fb"
-                    src={`https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FLingual-Ninja-491712431290062%2F&width=${width}&height=${width}&small_header=false&tabs=timeline$adapt_container_width=true&hide_cover=false&show_facepile=true&appId`}
+                    src={`https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FLingual-Ninja-491712431290062%2F&width=${width}&height=${height}&small_header=false&tabs=timeline$adapt_container_width=false&hide_cover=false&show_facepile=true&appId`}
                     width={width}
-                    height={width}
+                    height={height}
                     style={{ border: "none", overflow: "hidden" }}
                     scrolling="yes"
                     frameBorder="0"
