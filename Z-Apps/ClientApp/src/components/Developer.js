78e25085144fc8c808f7cd1fda23c0dfa5daa215
Kosuke_Zaizen <kosuke.zaizen@infodex.co.jp>
2020-01-08 10:22:32 +0900

    add scroll component to Developer and Terms

--- Z-Apps/ClientApp/src/components/Developer.js
+++ Z-Apps/ClientApp/src/components/Developer.js
@@ -3,6 +3,7 @@ import '../css/Developer.css';
 import image from '../img/KosukeZaizen.jpg';
 import Head from './parts/Helmet';
 import FB from './parts/FaceBook';
+import PleaseScrollDown from './parts/PleaseScrollDown';
 
 function SayHello() {
     return (
@@ -17,56 +18,69 @@ function SayHello() {
     );
 }
 
-const Developer = props => (
-    <div className="developer">
-        <Head
-            title="Kosuke Zaizen"
-            desc="I am a Japanese software engineer. Lingual Ninja is a website for Japanese learners. I hope Lingual Ninja can help!"
-        />
-        <center>
-            <h1>Kosuke Zaizen</h1>
 
-            <div className="contents">
-                <hr />
-                <span className='hidden-xs'>
-                    <table>
-                        <tbody>
-                            <tr>
-                                <td>
-                                    <img width="200px" src={image} alt="Kosuke Zaizen" />
-                                </td>
-                                <td className="tdExplanation" valign="top">
-                                    <SayHello />
-                                </td>
-                            </tr>
-                        </tbody>
-                    </table>
-                </span>
-                <span className='visible-xs'>
-                    <center>
-                        <img width="200px" src={image} alt="Kosuke Zaizen" />
-                        <br /><br />
-                        <SayHello />
-                    </center>
-                </span>
-                <hr />
-                <br />
+export default class Developer extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.ref = React.createRef();
+    }
+
+    render() {
+        return (
+            <div className="developer">
+                <Head
+                    title="Kosuke Zaizen"
+                    desc="I am a Japanese software engineer. Lingual Ninja is a website for Japanese learners. I hope Lingual Ninja can help!"
+                />
                 <center>
-                    <p className="no-margin">
-                        To contact me, please write a message<span className='hidden-xs'> </span><span className='visible-xs'><br /></span>
-                        using the link below:</p><br />
-                    <b><a href="https://uni-browser.lingual-ninja.com/?pageId=4" target="_blank" rel="noopener noreferrer">Contact uni-browser >></a></b>
-                        
-                    <br /><br /><br />
-                    <p className="no-margin">
-                        Also, I am writing a blog for people<span className='hidden-xs'> </span><span className='visible-xs'><br /></span>
-                        studying Japanese:</p><br />
-                        <b><a href="https://www.lingual-ninja.com/" target="_blank" rel="noopener noreferrer">Lingual Ninja! >></a></b>
-                    <br /><br />
-                    <FB />
+                    <h1>Kosuke Zaizen</h1>
+
+                    <div className="contents">
+                        <hr id="scrollTargetId" />
+                        <span className='hidden-xs'>
+                            <table>
+                                <tbody>
+                                    <tr>
+                                        <td>
+                                            <img width="200px" src={image} alt="Kosuke Zaizen" />
+                                        </td>
+                                        <td className="tdExplanation" valign="top">
+                                            <SayHello />
+                                        </td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </span>
+                        <span className='visible-xs'>
+                            <center>
+                                <img width="200px" src={image} alt="Kosuke Zaizen" />
+                                <br /><br />
+                                <SayHello />
+                            </center>
+                        </span>
+                        <hr ref={this.ref} />
+                        <br />
+                        <center>
+                            <p className="no-margin">
+                                To contact me, please write a message<span className='hidden-xs'> </span><span className='visible-xs'><br /></span>
+                                using the link below:</p><br />
+                            <b><a href="https://uni-browser.lingual-ninja.com/?pageId=4" target="_blank" rel="noopener noreferrer">Contact uni-browser >></a></b>
+
+                            <br /><br /><br />
+                            <p className="no-margin">
+                                Also, I am writing a blog for people<span className='hidden-xs'> </span><span className='visible-xs'><br /></span>
+                                studying Japanese:</p><br />
+                            <b><a href="https://www.lingual-ninja.com/" target="_blank" rel="noopener noreferrer">Lingual Ninja! >></a></b>
+                            <br /><br />
+                            <FB />
+                        </center>
+                    </div>
+                    <PleaseScrollDown
+                        criteriaRef={this.ref}
+                    />
                 </center>
-            </div>
-        </center>
-    </div >
-);
-export default Developer;
+            </div >
+        );
+    }
+}
--- Z-Apps/ClientApp/src/components/Terms.js
+++ Z-Apps/ClientApp/src/components/Terms.js
@@ -2,31 +2,43 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import '../css/Terms.css';
 import Head from './parts/Helmet';
+import PleaseScrollDown from './parts/PleaseScrollDown';
 
-const Terms = props => (
-    <div className="terms">
-        <Head
-            title="Terms of Use"
-            desc="Lingual Ninja - The ownership of website and Responsibility"
-        />
-        <center>
-            <h1>Lingual Ninja<span className='hidden-xs'> </span><span className='visible-xs'><br /></span>Terms of Use</h1>
-            <div className="contents">
-                <hr />
-                <h2>The ownership of website</h2>
-                <p>This website is developed and owned by <Link to="/developer">Kosuke Zaizen</Link>.
+export default class Terms extends React.Component {
+    constructor(props) {
+        super(props);
+        this.ref = React.createRef();
+    }
+
+    render() {
+        return (
+            <div className="terms">
+                <Head
+                    title="Terms of Use"
+                    desc="Lingual Ninja - The ownership of website and Responsibility"
+                />
+                <center>
+                    <h1 id="scrollTargetId">Lingual Ninja<span className='hidden-xs'> </span><span className='visible-xs'><br /></span>Terms of Use</h1>
+                    <div className="contents">
+                        <hr />
+                        <h2>The ownership of website</h2>
+                        <p>This website is developed and owned by <Link to="/developer">Kosuke Zaizen</Link>.
                 When you want to use any quotes, images, or programs, you must get approval from the owner.</p>
-                <hr />
-                <h2>Responsibility</h2>
-                <p>If user experiences trouble including defects or bugs, 
-                    the owner of this website can't be held liable.
+                        <hr ref={this.ref} />
+                        <h2>Responsibility</h2>
+                        <p>If user experiences trouble including defects or bugs,
+                            the owner of this website can't be held liable.
                     It will be user's responsibility.</p>
-                <hr />
-                <h2>Contact</h2>
-                If you have trouble, please contact using this link:<br />
-                <a href="https://uni-browser.lingual-ninja.com/?pageId=4" target="_blank" rel="noopener noreferrer">　uni-browser >></a>
+                        <hr />
+                        <h2>Contact</h2>
+                        If you have trouble, please contact using this link:<br />
+                        <a href="https://uni-browser.lingual-ninja.com/?pageId=4" target="_blank" rel="noopener noreferrer">　uni-browser >></a>
+                    </div>
+                    <PleaseScrollDown
+                        criteriaRef={this.ref}
+                    />
+                </center>
             </div>
-        </center>
-    </div>
-);
-export default Terms;
+        );
+    }
+}
