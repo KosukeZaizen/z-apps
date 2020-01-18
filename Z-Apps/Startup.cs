f2c1b05108983be6d871ac56f38aaefa229879b0
Kosuke Zaizen <kosukezaizen@yahoo.co.jp>
2020-01-18 21:07:10 +0900

    ogp setting

--- Z-Apps/Startup.cs
+++ Z-Apps/Startup.cs
@@ -1,10 +1,13 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.SpaServices.ReactDevelopmentServer;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Z_Apps.Models;
+using Z_Apps.Models.Stories.Stories;
 
 namespace Z_Apps
 {
@@ -46,6 +49,74 @@ namespace Z_Apps
             app.UseHttpsRedirection();
             app.UseStaticFiles();
             app.UseSpaStaticFiles();
+
+            app.Use(async (context, next) =>
+            {
+                var ua = context.Request.Headers["User-Agent"].ToString();
+
+                if (ua.StartsWith("facebookexternalhit") || ua.StartsWith("Twitterbot"))
+                {
+
+                    string url = context.Request.Path.Value;
+                    if (url == null)
+                    {
+                        await next.Invoke();
+                    }
+                    else if (url == "/")
+                    {
+                        await context.Response.WriteAsync("" +
+                            "<head prefix='og: http://ogp.me/ns# fb: http://ogp.me/ns/fb# website: http://ogp.me/ns/website#'>" +
+                            "<meta property='og:image' content='https://z-apps.lingual-ninja.com/ogp-img.png' />" +
+                            "<meta property='og:url' content='" + "https://z-apps.lingual-ninja.com' />" +
+                            "<meta property='og:type' content='website' />" +
+                            "<meta property='og:title' content='Lingual Ninja' />" +
+                            "<meta property='og:description' content='Applications to learn Japanese! You can study Japanese from Japanese folktales!' />" +
+                            "<meta property='og:site_name' content='Lingual Ninja' />" +
+                            "<meta name='twitter:card' content='summary'>" +
+                            "<meta name='twitter:site' content='@LingualNinja'>" +
+                            "</head>");
+                    }
+                    else if (url.Contains("folktales/"))
+                    {
+                        string storyName = url.Split("folktales/")[1].Replace("/", "");
+
+                        var storyManager = new StoryManager(new DBCon());
+                        var story = storyManager.GetStory(storyName);
+                        var description = story.Description;
+
+                        await context.Response.WriteAsync("" +
+                                "<head prefix='og: http://ogp.me/ns# fb: http://ogp.me/ns/fb# article: http://ogp.me/ns/article#'>" +
+                                "<meta property='og:image' content='https://z-apps.lingual-ninja.com/imgs/" + storyName.Split("--")[0] + ".png' />" +
+                                "<meta property='og:url' content='" + "https://z-apps.lingual-ninja.com" + url + "' />" +
+                                "<meta property='og:type' content='article' />" +
+                                "<meta property='og:title' content='"+storyName.Replace("--", " - ").Replace("_"," ")+"' />" +
+                                "<meta property='og:description' content='" + description + "' />" +
+                                "<meta property='og:site_name' content='Lingual Ninja' />" +
+                                "<meta name='twitter:card' content='summary'>" +
+                                "<meta name='twitter:site' content='@LingualNinja'>" +
+                                "</head>");
+
+                    }
+                    else
+                    {
+                        await context.Response.WriteAsync("" +
+                                "<head prefix='og: http://ogp.me/ns# fb: http://ogp.me/ns/fb# article: http://ogp.me/ns/article#'>" +
+                                "<meta property='og:image' content='https://z-apps.lingual-ninja.com/ogp-img.png' />" +
+                                "<meta property='og:url' content='" + "https://z-apps.lingual-ninja.com" + url + "' />" +
+                                "<meta property='og:type' content='article' />" +
+                                "<meta property='og:title' content='Lingual Ninja' />" +
+                                "<meta property='og:description' content='Applications to learn Japanese! You can study Japanese from Japanese folktales!' />" +
+                                "<meta property='og:site_name' content='Lingual Ninja' />" +
+                                "<meta name='twitter:card' content='summary'>" +
+                                "<meta name='twitter:site' content='@LingualNinja'>" +
+                                "</head>");
+                    }
+                }
+                else
+                {
+                    await next.Invoke();
+                }
+            });
 
             app.UseMvc(routes =>
             {
