045d76c82a9da09f0d924e72b20e5518455c9f1c
Kosuke Zaizen <kosukezaizen@yahoo.co.jp>
2020-02-03 08:35:52 +0900

    add random digits to userId

--- Z-Apps/Models/SystemBase/LogService.cs
+++ Z-Apps/Models/SystemBase/LogService.cs
@@ -20,7 +20,11 @@ namespace Z_Apps.Models.SystemBase
             {
                 if (data.token == PrivateConsts.LOG_TOKEN)
                 {
-                    if (data.userId == "0") data.userId = DateTime.Now.ToString("yyyyMMddhhmmssfff");
+                    if (data.userId == "0")
+                    {
+                        var rand = new Random();
+                        data.userId = DateTime.Now.ToString("yyyyMMddhhmmssfff") + rand.Next(0, 10) + rand.Next(0, 10) + rand.Next(0, 10);
+                    }
 
                     string accessLog = "{userId:" + data.userId + ",href:\"" + data.href + "\"}";
                     logger.LogWarning(accessLog);
