a1c3505134d191a6062124a1749e886662a0cc60
Kosuke Zaizen <kosukezaizen@yahoo.co.jp>
2020-03-25 23:16:49 +0900

    remove bk

--- Z-Apps/ClientApp/src-bk/components/common/consts.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.TOP_URL = 'https://z-apps.lingual-ninja.com';
-exports.BLOB_URL = 'https://lingualninja.blob.core.windows.net/lingual-storage';
