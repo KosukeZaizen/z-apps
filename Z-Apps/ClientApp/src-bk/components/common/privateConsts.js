ef254fe5179532b21057ef6a968895a8c8a3e2ca
Kosuke Zaizen <kosukezaizen@yahoo.co.jp>
2020-03-25 22:55:29 +0900

    simplify typescript

---
+++ Z-Apps/ClientApp/src-bk/components/common/privateConsts.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.GOOGLE_ANALYTICS = 'UA-122613642-6';
+exports.GOOGLE_ADS_CLIENT = "ca-pub-8006404698960678";
+exports.GOOGLE_ADS_SLOT = "2560074548";
+exports.LOG_TOKEN = "d5(TaY(G*Z*C6T88_bz)9n6rus.vN4H-/gufqntxmg128";
