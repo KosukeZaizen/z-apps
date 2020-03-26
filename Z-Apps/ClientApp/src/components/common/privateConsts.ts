5f533b41bb1dc2515f1227a3e9dc977d02c3677c
Kosuke_Zaizen <kosuke.zaizen@infodex.co.jp>
2020-03-26 18:48:50 +0900

    no message

--- Z-Apps/ClientApp/src/components/common/privateConsts.ts
+++
@@ -1,4 +0,0 @@
-export const GOOGLE_ANALYTICS = 'UA-122613642-6';
-export const GOOGLE_ADS_CLIENT = "ca-pub-8006404698960678";
-export const GOOGLE_ADS_SLOT = "2560074548";
-export const LOG_TOKEN = "d5(TaY(G*Z*C6T88_bz)9n6rus.vN4H-/gufqntxmg128";
