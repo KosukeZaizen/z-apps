58dbccbf7a8d631f8c4c667dfefcd00a24afd588
Kosuke_Zaizen <kosuke.zaizen@infodex.co.jp>
2020-04-02 12:07:25 +0900

    reactivate ServiceWorker

--- Z-Apps/ClientApp/src/index.tsx
+++ Z-Apps/ClientApp/src/index.tsx
@@ -7,8 +7,8 @@ import { ConnectedRouter } from 'react-router-redux';
 import { createBrowserHistory } from 'history';
 import configureStore from './store/configureStore';
 import App from './App';
-//import registerServiceWorker from './registerServiceWorker';
-import { unregister } from './registerServiceWorker';
+import registerServiceWorker from './registerServiceWorker';
+//import { unregister } from './registerServiceWorker';
 import ReactGA from 'react-ga';
 import { GOOGLE_ANALYTICS } from './components/common/privateConsts';
 
@@ -37,5 +37,5 @@ ReactDOM.render(
   </Provider>,
   rootElement);
 
-//registerServiceWorker();
-unregister();
+registerServiceWorker();
+//unregister();
