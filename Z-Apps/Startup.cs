7671f5846946346a9029abc995379e30eddb217e
Kosuke_Zaizen <kosukezaizen@yahoo.co.jp>
2020-04-13 08:56:55 +0900

    sns bot

--- Z-Apps/Startup.cs
+++ Z-Apps/Startup.cs
@@ -165,6 +165,52 @@ namespace Z_Apps
                                     "</head>" + Environment.NewLine +
                                     "<body>Content for SNS bot</body>";
                         }
+                        else if(url.Contains("kanji-quiz"))
+                        {
+                            var arrUrl = url.Split("/");
+                            var lastElem = arrUrl.LastOrDefault();
+                            string title = (lastElem == "kanji-quiz") ?
+                                "Kanji Quiz" : 
+                                "Kanji Quiz - " + string.Join(" ", lastElem.Split("_").Select((e) => System.Globalization.CultureInfo.CurrentCulture.TextInfo.ToTitleCase(e)));
+
+                            resultHTML = "" +
+                                    "<head>" +
+                                    "<meta name='twitter:card' content='summary'>" + Environment.NewLine +
+                                    "<meta name='twitter:site' content='@LingualNinja'>" + Environment.NewLine +
+                                    "<meta property='og:image' content='https://z-apps.lingual-ninja.com/ogp-img.png'>" + Environment.NewLine +
+                                    "<meta property='og:url' content='https://z-apps.lingual-ninja.com" + url + "'>" + Environment.NewLine +
+                                    "<meta property='og:type' content='article'>" + Environment.NewLine +
+                                    "<meta property='og:title' content='" + title + "'>" + Environment.NewLine +
+                                    "<meta property='og:image:alt' content='Lingual Ninja'>" + Environment.NewLine +
+                                    "<meta property='og:description' content='Free app to learn Japanese Kanji characters! Try to get a perfect score on all the quizzes!'>" + Environment.NewLine +
+                                    "<meta property='og:site_name' content='Lingual Ninja'>" + Environment.NewLine +
+                                    "<meta property='fb:app_id' content='217853132566874'>" + Environment.NewLine +
+                                    "<meta property='fb:page_id' content='491712431290062'>" + Environment.NewLine +
+                                    "</head>" + Environment.NewLine +
+                                    "<body>Content for SNS bot</body>";
+                        }
+                        else if(url.Contains("vocabulary-list"))
+                        {
+                            var arrUrl = url.Split("/");
+                            var lastElem = arrUrl.LastOrDefault();
+                            string title = "Japanese Vocabulary List";
+
+                            resultHTML = "" +
+                                    "<head>" +
+                                    "<meta name='twitter:card' content='summary'>" + Environment.NewLine +
+                                    "<meta name='twitter:site' content='@LingualNinja'>" + Environment.NewLine +
+                                    "<meta property='og:image' content='https://z-apps.lingual-ninja.com/ogp-img.png'>" + Environment.NewLine +
+                                    "<meta property='og:url' content='https://z-apps.lingual-ninja.com" + url + "'>" + Environment.NewLine +
+                                    "<meta property='og:type' content='article'>" + Environment.NewLine +
+                                    "<meta property='og:title' content='" + title + "'>" + Environment.NewLine +
+                                    "<meta property='og:image:alt' content='Lingual Ninja'>" + Environment.NewLine +
+                                    "<meta property='og:description' content='Free app to learn Japanese vocabulary! Try to get a perfect score on all the quizzes!'>" + Environment.NewLine +
+                                    "<meta property='og:site_name' content='Lingual Ninja'>" + Environment.NewLine +
+                                    "<meta property='fb:app_id' content='217853132566874'>" + Environment.NewLine +
+                                    "<meta property='fb:page_id' content='491712431290062'>" + Environment.NewLine +
+                                    "</head>" + Environment.NewLine +
+                                    "<body>Content for SNS bot</body>";
+                        }
                         else
                         {
                             resultHTML = "" +
--- Z-Apps/Util/Consts.cs
+++ Z-Apps/Util/Consts.cs
@@ -3,7 +3,7 @@ namespace Z_Apps.Util
     public class Consts
     {
         //app version
-        public static int APP_VERSION = 3;
+        public static int APP_VERSION = 4;
 
         //site url
         public static string BLOB_URL = "https://lingualninja.blob.core.windows.net";
