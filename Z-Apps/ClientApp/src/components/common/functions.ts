2f25548833086ae42e3584d1c01aaf1c4fe2bbb2
Kosuke_Zaizen <kosukezaizen@yahoo.co.jp>
2020-04-13 07:58:58 +0900

    redirect immadiately

--- Z-Apps/ClientApp/src/components/common/functions.ts
+++ Z-Apps/ClientApp/src/components/common/functions.ts
@@ -82,7 +82,8 @@ export function reloadAndRedirect_OneTimeReload(saveKey: string) {
     //★広告表示等のため、リロード連打がまずい場合はこちらを使う
     //初回はリロードし、時刻記録
     //その後、1回だけリロード
-    //その後、10秒後に404ページにリダイレクト
+    //その後、その後、404ページにリダイレクト
+    //存在してはいけないページがIndexされないため、２回目は即時リダイレクトを行う
 
     const savedErrTime = window.sessionStorage.getItem(saveKey);
     const intSavedTime = parseInt(savedErrTime);
@@ -91,9 +92,7 @@ export function reloadAndRedirect_OneTimeReload(saveKey: string) {
     const nowTime = now.getTime();
 
     if (intSavedTime && (nowTime - intSavedTime < 15000)) {
-        setTimeout(() => {
-            window.location.href = `/not-found?p=${window.location.pathname}`;
-        }, 10000);
+        window.location.href = `/not-found?p=${window.location.pathname}`;
     } else {
         window.sessionStorage.setItem(saveKey, nowTime.toString());
         window.location.reload(true);
