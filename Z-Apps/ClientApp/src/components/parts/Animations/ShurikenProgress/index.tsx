a2cdce643e048a3f83216b291c417bc820dd8269
Kosuke_Zaizen <kosukezaizen@yahoo.co.jp>
2020-10-18 19:39:27 +0900

    ローディングアニメーションを手裏剣のみに変更

--- Z-Apps/ClientApp/src/components/parts/Animations/ShurikenProgress/index.tsx
+++ Z-Apps/ClientApp/src/components/parts/Animations/ShurikenProgress/index.tsx
@@ -1,6 +1,4 @@
-import { CircularProgress } from "@material-ui/core";
 import * as React from "react";
-import { useEffect, useRef, useState } from "react";
 import "./animation.css";
 
 const shuriken = require("../../../../img/shuriken.png");
@@ -9,33 +7,13 @@ interface Props {
     size?: string;
 }
 export default function ShurikenProgress({ size }: Props) {
-    const [element, setElement] = useState<HTMLDivElement | null>(null);
-    const ref = useRef(null);
-    useEffect(() => {
-        const el = ref.current as HTMLDivElement | null;
-        el && setElement(el);
-    }, []);
-    const shortLength =
-        (element && Math.min(element.clientWidth, element.clientHeight)) || 0;
-
-    const rect = element?.getBoundingClientRect();
     return (
-        <div>
-            <CircularProgress
-                style={{
-                    position: "absolute",
-                    left: rect?.left,
-                    top: rect?.top,
-                    width: shortLength,
-                    height: shortLength,
-                }}
-            />
+        <div style={{ width: size, height: size }}>
             <img
-                ref={ref}
                 src={shuriken}
                 alt="shuriken"
                 className="ShurikenProgress"
-                style={{ width: size, height: size }}
+                style={{ width: "100%", height: "100%" }}
             />
         </div>
     );
