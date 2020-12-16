0454b9b72d61f5f6e41c316815747cb959eb6dde
Kosuke_Zaizen <kosukezaizen@yahoo.co.jp>
2020-12-16 21:12:21 +0900

    紅葉のコンソールエラー

--- Z-Apps/ClientApp/src/components/parts/Animations/Momiji/index.tsx
+++ Z-Apps/ClientApp/src/components/parts/Animations/Momiji/index.tsx
@@ -49,7 +49,7 @@ export const Momiji = ({ frequencySec, screenWidth }: Props) => {
             ls = [];
             clearInterval(intervalId);
         };
-    }, []);
+    }, [frequencySec]);
 
     return (
         <div>
