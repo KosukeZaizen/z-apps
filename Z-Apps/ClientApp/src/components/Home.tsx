acdfd9d140c642e327ddcf70d5a980db7ae380be
Kosuke_Zaizen <kosukezaizen@yahoo.co.jp>
2020-12-28 18:13:11 +0900

    FolktaleMenuの別ファイル切り出し

--- Z-Apps/ClientApp/src/components/Home.tsx
+++ Z-Apps/ClientApp/src/components/Home.tsx
@@ -1,5 +1,4 @@
 import * as React from "react";
-import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { Button, Card, CardText, CardTitle } from "reactstrap";
 import "../css/Home.css";
@@ -7,9 +6,9 @@ import { SeasonAnimation } from "./parts/Animations/SeasonAnimation";
 import { Author } from "./parts/Author";
 import CharacterComment from "./parts/CharacterComment";
 import FB from "./parts/FaceBook";
+import { FolktaleMenu } from "./parts/FolktaleMenu";
 import Head from "./parts/Helmet";
 import PleaseScrollDown from "./parts/PleaseScrollDown";
-import { ScrollBox } from "./parts/ScrollBox";
 
 export default class Home extends React.Component<
     {},
@@ -356,85 +355,3 @@ export default class Home extends React.Component<
         );
     }
 }
-
-interface FolktaleMenuProps {
-    screenWidth: number;
-}
-const buttonColor = { 1: "secondary", 2: "success", 3: "primary" };
-type ButtonKey = keyof typeof buttonColor;
-export const FolktaleMenu = ({ screenWidth }: FolktaleMenuProps) => {
-    const isWide = screenWidth > 991;
-    const [buttonKey, setButtonKey] = useState<ButtonKey>(1);
-
-    useEffect(() => {
-        const timerId = window.setTimeout(() => {
-            const nextKey = (buttonKey - 1 || 3) as ButtonKey;
-            setButtonKey(nextKey);
-        }, 3000);
-        return () => clearTimeout(timerId);
-    }, [buttonKey]);
-
-    return (
-        <ScrollBox style={{ textAlign: "center" }}>
-            <Link to="/folktales">
-                <h2>Japanese Folktales</h2>
-            </Link>
-            <div
-                style={{
-                    display: "flex",
-                    flexDirection: isWide ? "row" : "column",
-                }}
-            >
-                <div>
-                    <Link to="/folktales">
-                        <img
-                            style={{ width: "100%" }}
-                            src={
-                                "https://lingualninja.blob.core.windows.net/lingual-storage/folktalesImg/Momotaro.png"
-                            }
-                            alt="Japanese Folktale Momotaro"
-                        />
-                    </Link>
-                </div>
-                <div>
-                    <div
-                        style={{
-                            fontSize: "large",
-                            textAlign: "left",
-                            padding: isWide ? 25 : "10px 10px 20px",
-                        }}
-                    >
-                        A web app to learn Japanese from folktales. You can read
-                        traditional Japanese folktales in English, Hiragana,
-                        Kanji, and Romaji!
-                    </div>
-                    <div
-                        style={{
-                            display: "flex",
-                            justifyContent: "center",
-                            alignItems: "center",
-                        }}
-                    >
-                        <Link
-                            to="/folktales"
-                            style={{
-                                fontWeight: "bold",
-                            }}
-                        >
-                            <Button
-                                size="lg"
-                                color={buttonColor[buttonKey]}
-                                style={{
-                                    width: 100,
-                                }}
-                                className="colorChangeButton"
-                            >
-                                Try!
-                            </Button>
-                        </Link>
-                    </div>
-                </div>
-            </div>
-        </ScrollBox>
-    );
-};
---
+++ Z-Apps/ClientApp/src/components/parts/FolktaleMenu/index.tsx
@@ -0,0 +1,87 @@
+import * as React from "react";
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { Button } from "reactstrap";
+import { ScrollBox } from "../ScrollBox";
+
+interface FolktaleMenuProps {
+    screenWidth: number;
+}
+const buttonColor = { 1: "secondary", 2: "success", 3: "primary" };
+type ButtonKey = keyof typeof buttonColor;
+export const FolktaleMenu = ({ screenWidth }: FolktaleMenuProps) => {
+    const isWide = screenWidth > 991;
+    const [buttonKey, setButtonKey] = useState<ButtonKey>(1);
+
+    useEffect(() => {
+        const timerId = window.setTimeout(() => {
+            const nextKey = (buttonKey - 1 || 3) as ButtonKey;
+            setButtonKey(nextKey);
+        }, 3000);
+        return () => clearTimeout(timerId);
+    }, [buttonKey]);
+
+    return (
+        <ScrollBox style={{ textAlign: "center" }}>
+            <Link to="/folktales">
+                <h2>Japanese Folktales</h2>
+            </Link>
+            <div
+                style={{
+                    display: "flex",
+                    flexDirection: isWide ? "row" : "column",
+                }}
+            >
+                <div>
+                    <Link to="/folktales">
+                        <img
+                            style={{ width: "100%" }}
+                            src={
+                                "https://lingualninja.blob.core.windows.net/lingual-storage/folktalesImg/Momotaro.png"
+                            }
+                            alt="Japanese Folktale Momotaro"
+                        />
+                    </Link>
+                </div>
+                <div>
+                    <div
+                        style={{
+                            fontSize: "large",
+                            textAlign: "left",
+                            padding: isWide ? 25 : "10px 10px 20px",
+                        }}
+                    >
+                        A web app to learn Japanese from folktales. You can read
+                        traditional Japanese folktales in English, Hiragana,
+                        Kanji, and Romaji!
+                    </div>
+                    <div
+                        style={{
+                            display: "flex",
+                            justifyContent: "center",
+                            alignItems: "center",
+                        }}
+                    >
+                        <Link
+                            to="/folktales"
+                            style={{
+                                fontWeight: "bold",
+                            }}
+                        >
+                            <Button
+                                size="lg"
+                                color={buttonColor[buttonKey]}
+                                style={{
+                                    width: 100,
+                                }}
+                                className="colorChangeButton"
+                            >
+                                Try!
+                            </Button>
+                        </Link>
+                    </div>
+                </div>
+            </div>
+        </ScrollBox>
+    );
+};
