57d7e20919493df03789805f3643a109bb2d61e4
Kosuke_Zaizen <kosukezaizen@yahoo.co.jp>
2021-01-16 13:30:49 +0900

    WelcomeAnimationを短く

--- Z-Apps/ClientApp/src/components/parts/Animations/WelcomeAnimation/index.tsx
+++ Z-Apps/ClientApp/src/components/parts/Animations/WelcomeAnimation/index.tsx
@@ -1,11 +1,7 @@
 import * as React from "react";
 import { useEffect, useState } from "react";
 import { AnimationEngine } from "../../../../common/animation";
-import { appsPublicImg } from "../../../../common/consts";
 import "./animation.css";
-
-const shuriken = appsPublicImg + "shuriken.png";
-const runningNinja = appsPublicImg + "ninja_hashiru.png";
 
 export let finishWelcomeAnimation: () => void = () => {};
 
@@ -39,16 +35,16 @@ export default function WelcomeAnimation() {
                 if (time === 1) {
                     isOpen = true;
                 }
-                if (time > 95 && time <= 125) {
+                if (time > 15 && time <= 45) {
                     underBarOpacity = 1;
                     underBarLength = underBarLength + 100;
                 }
-                if (time > 125) {
+                if (time > 45) {
                     if (underBarOpacity > 0) {
                         underBarOpacity = underBarOpacity - 0.04;
                     }
                 }
-                if (time > 215) {
+                if (time > 90) {
                     shown = false;
                     animation.cleanUpAnimation();
                 }
@@ -118,35 +114,13 @@ export default function WelcomeAnimation() {
                     fontSize: charHeight,
                     padding: 0,
                     fontWeight: "bold",
-                    transition: "2s",
+                    transition: "1s",
                     opacity: animationState.isOpen ? 1 : 0,
                     fontStyle: "italic",
                 }}
             >
                 {"Lingual Ninja"}
             </p>
-            <img
-                src={runningNinja}
-                alt={"running ninja"}
-                style={{
-                    width: 210 * U,
-                    position: "absolute",
-                    left: leftTopPosition[0] + 100 * U,
-                    top: leftTopPosition[1] + 470 * U,
-                }}
-                className="ninja"
-            />
-            <img
-                src={shuriken}
-                alt={"shuriken"}
-                style={{
-                    width: 170 * U,
-                    position: "absolute",
-                    left: leftTopPosition[0] + 750 * U,
-                    top: leftTopPosition[1] + 270 * U,
-                }}
-                className="shuriken"
-            />
         </div>
     );
 }
