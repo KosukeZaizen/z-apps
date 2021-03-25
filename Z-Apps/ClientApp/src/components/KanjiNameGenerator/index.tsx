ce830a9b93a3fbe5dd9b229b22168d5b78f4e92c
Kosuke_Zaizen <kosuke.zaizen@infodex.co.jp>
2021-03-25 11:08:37 +0900

    Revert "Kanji Name Generatorの入力欄作成"
    This reverts commit 0b6c47330a06855787d3d13fb8c5559f20bc07e3.

--- Z-Apps/ClientApp/src/components/KanjiNameGenerator/index.tsx
+++ Z-Apps/ClientApp/src/components/KanjiNameGenerator/index.tsx
@@ -1,54 +1,5 @@
-import { Input, Slide } from "@material-ui/core";
 import * as React from "react";
-import { useState } from "react";
 
 export default function KanjiNameGenerator() {
-    const [english, setEnglish] = useState("");
-    const [errMsg, setErrMsg] = useState("");
-    return (
-        <div style={{ maxWidth: 700 }}>
-            <h1>{"Kanji Name Generator - My Name in Kanji"}</h1>
-            <p
-                style={{
-                    marginTop: 50,
-                    fontSize: "x-large",
-                    fontWeight: "bold",
-                }}
-            >
-                Your name:
-            </p>
-            <Input
-                onChange={e => {
-                    const val = e.target.value;
-                    setEnglish(val);
-                    setErrMsg(chkAlphabets(val));
-                }}
-                value={english}
-                style={{ display: "block", color: errMsg ? "red" : undefined }}
-                placeholder={"Type your name in alphabet here"}
-            />
-            {errMsg && <p style={{ color: "red", marginTop: 5 }}>{errMsg}</p>}
-            <Slide in={!!english && !errMsg} direction="left">
-                <div>
-                    <p
-                        style={{
-                            marginTop: 50,
-                            fontSize: "x-large",
-                            fontWeight: "bold",
-                        }}
-                    >
-                        Kanji:
-                    </p>
-                    {english.split("a").join("あ")}
-                </div>
-            </Slide>
-        </div>
-    );
-}
-
-function chkAlphabets(str: string): string {
-    if (!new RegExp(/^[a-zA-Z\s]*$/).test(str)) {
-        return "Please use only alphabet";
-    }
-    return "";
+    return <div>hello</div>;
 }
