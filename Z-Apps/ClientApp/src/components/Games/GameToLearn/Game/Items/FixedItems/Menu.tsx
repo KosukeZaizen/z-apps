550de73e9aa6dd15324c03d78c5b2b60ca4bc557
Kosuke_Zaizen <kosuke.zaizen@infodex.co.jp>
2021-04-02 19:15:02 +0900

    コンポーネントを分ける

--- Z-Apps/ClientApp/src/components/Games/GameToLearn/Game/Items/FixedItems/Menu.tsx
+++ Z-Apps/ClientApp/src/components/Games/GameToLearn/Game/Items/FixedItems/Menu.tsx
@@ -62,68 +62,122 @@ function GameMenu({
     return (
         <>
             {UL && (
-                <Button
-                    variant="contained"
-                    color={open ? "secondary" : "primary"}
-                    style={{
-                        zIndex: 20005,
-                        position: "absolute",
-                        top: 1 * UL,
-                        left: 139 * UL,
-                        width: 20 * UL,
-                        height: 6 * UL,
-                        fontSize: 3 * UL,
-                        fontWeight: "bold",
-                        opacity: open ? 1 : 0.9,
-                        transitionDuration: gameOpenAnimationTime,
-                    }}
+                <MenuButton
+                    UL={UL}
+                    open={open}
                     onClick={() => setOpen(!open)}
-                >
-                    {open ? "Close" : "Menu"}
-                </Button>
+                />
             )}
+            <MenuScreen
+                UL={UL}
+                open={open}
+                isChildrenMounted={isChildrenMounted}
+            >
+                {children}
+            </MenuScreen>
+            <BlackLayer open={open} UL={UL} onClick={() => setOpen(false)} />
+        </>
+    );
+}
+
+function MenuButton({
+    UL,
+    open,
+    onClick,
+}: {
+    UL: number;
+    open: boolean;
+    onClick: () => void;
+}) {
+    return (
+        <Button
+            variant="contained"
+            color={open ? "secondary" : "primary"}
+            style={{
+                zIndex: 20005,
+                position: "absolute",
+                top: 1 * UL,
+                left: 139 * UL,
+                width: 20 * UL,
+                height: 6 * UL,
+                fontSize: 3 * UL,
+                fontWeight: "bold",
+                opacity: open ? 1 : 0.9,
+                transitionDuration: gameOpenAnimationTime,
+            }}
+            onClick={onClick}
+        >
+            {open ? "Close" : "Menu"}
+        </Button>
+    );
+}
+
+function MenuScreen({
+    UL,
+    open,
+    isChildrenMounted,
+    children,
+}: {
+    UL: number;
+    open: boolean;
+    isChildrenMounted: boolean;
+    children: JSX.Element | JSX.Element[];
+}) {
+    return (
+        <div
+            style={{
+                position: "absolute",
+                top: 1 * UL,
+                right: open ? 21 * UL : 1,
+                width: open ? 138 * UL : 0,
+                height: open ? 88 * UL : 0,
+                transition: gameOpenAnimationTime,
+                zIndex: 20002,
+                backgroundColor: "white",
+                borderRadius: 3 * UL,
+                overflow: "hidden",
+            }}
+        >
+            <div
+                style={{
+                    marginTop: 3 * UL,
+                    width: 138 * UL,
+                    height: 82 * UL,
+                    overflowY: "scroll",
+                    opacity: open ? 1 : 0,
+                    transition: "500ms",
+                }}
+            >
+                {isChildrenMounted && children}
+            </div>
+        </div>
+    );
+}
+
+function BlackLayer({
+    UL,
+    open,
+    onClick,
+}: {
+    UL: number;
+    open: boolean;
+    onClick: () => void;
+}) {
+    return (
+        <Slide in={open} direction="down">
             <div
                 style={{
                     position: "absolute",
-                    top: 1 * UL,
-                    right: open ? 21 * UL : 1,
-                    width: open ? 138 * UL : 0,
-                    height: open ? 88 * UL : 0,
-                    transition: gameOpenAnimationTime,
-                    zIndex: 20002,
-                    backgroundColor: "white",
-                    borderRadius: 3 * UL,
-                    overflow: "hidden",
+                    top: 0,
+                    left: 0,
+                    width: 160 * UL,
+                    height: 90 * UL,
+                    zIndex: 20001,
+                    backgroundColor: "black",
+                    opacity: 0.5,
                 }}
-            >
-                <div
-                    style={{
-                        marginTop: 3 * UL,
-                        width: 138 * UL,
-                        height: 82 * UL,
-                        overflowY: "scroll",
-                        opacity: open ? 1 : 0,
-                        transition: "500ms",
-                    }}
-                >
-                    {isChildrenMounted && children}
-                </div>
-            </div>
-            <Slide in={open} direction="down">
-                <div
-                    style={{
-                        position: "absolute",
-                        top: 0,
-                        left: 0,
-                        width: 160 * UL,
-                        height: 90 * UL,
-                        zIndex: 20001,
-                        backgroundColor: "black",
-                        opacity: 0.5,
-                    }}
-                    onClick={() => setOpen(false)}
-                />
-            </Slide>
-        </>
+                onClick={onClick}
+            />
+        </Slide>
     );
 }
